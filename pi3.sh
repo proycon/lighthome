7899a0e4a002101b587df94dc736970a99e092af
Maarten van Gompel <proycon@anaproy.nl>
2022-08-16 23:51:04 +0200

    pi3: fix

--- pi3.sh
+++ pi3.sh
@@ -17,11 +17,11 @@ mqtt_transmitter "home/sensor/mhz19_co2" 60 mhz19
 
 export GPIO_PIN_DHT22=4
 export DHT22_MODE=c
-mqtt_transmitter "home/sensor/temperature_living_room" 60 mhz19
+mqtt_transmitter "home/sensor/temperature_living_room" 60 dht22
 
 export GPIO_PIN_DHT22=4
 export DHT22_MODE=h
-mqtt_transmitter "home/sensor/humidity_living_room" 360 mhz19
+mqtt_transmitter "home/sensor/humidity_living_room" 360 dht22
 
 
 wait #wait for all children
