4d744504f918857e9788d588a3f29214c8d6bf23
Maarten van Gompel <proycon@anaproy.nl>
2022-08-16 19:37:58 +0200

    gpio_binary_sensor: added flush

--- programs/gpio_binary_sensor.c
+++ programs/gpio_binary_sensor.c
@@ -69,6 +69,7 @@ int main(int argc, char *argv[]) {
         state[0] = digitalRead(pin);
         if ((state[0] == state[1]) && (state[1] != state[2])) {
             printf("%d\n", state[0]);
+            fflush(stdout);
         }
         for (int i = 2; i > 0; i--) state[i] = state[i-1];
         delay(60);
