6089e6fa64229c8360f67c7c04afb06452e63871
Maarten van Gompel <proycon@anaproy.nl>
2022-08-16 22:06:09 +0200

    fix

--- scripts/technofire/technofire.sh
+++ scripts/technofire/technofire.sh
@@ -6,7 +6,7 @@ fi
 . "$HAROOT/scripts/common/include.sh"
 
 havedep pkill
-havedpe "$HAROOT/scripts/technofire/technofire.py"
+havedep "$HAROOT/scripts/technofire/technofire.py"
 
 pkill -f technofire.py > /dev/null 2>/dev/null
 $HAROOT/scripts/technofire/technofire.py $@
