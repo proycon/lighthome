01bef858bde3e52ab1117d2f00aadb174b6c1971
Maarten van Gompel <proycon@anaproy.nl>
2022-08-20 17:39:07 +0200

    statefiles: fix

--- scripts/mqtthandlers/statefiles.sh
+++ scripts/mqtthandlers/statefiles.sh
@@ -9,7 +9,7 @@ handle_statefiles() {
         "home/sensor/"*|"home/binary_sensor/"*)
             TYPE=$(echo "$TOPIC" | cut -d "/" -f 2)
             SENSOR=$(echo "$TOPIC" | cut -d "/" -f 3)
-            [ -e "$HASTATEDIR/$TYPE" ] && mkdir -p "$HASTATEDIR/$TYPE"
+            [ ! -e "$HASTATEDIR/$TYPE" ] && mkdir -p "$HASTATEDIR/$TYPE"
             echo "$PAYLOAD" > "$HASTATEDIR/$TYPE/$SENSOR"
             return 0
             ;;
