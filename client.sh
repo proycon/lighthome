6a0cc4e7954428ed73c85104248e6617de417119
Maarten van Gompel <proycon@anaproy.nl>
2022-08-26 15:24:53 +0200

    added generic client for sounds/notifications

---
+++ client.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ -z "$HAROOT" ]; then
+    export HAROOT="$(dirname $(realpath "$0"))"
+fi
+export PLAY="mpv --no-video --really-quiet"
+
+. "$HAROOT/scripts/common/include.sh"
+
+settrap #kill all children when dying
+
+#runs asynchronously
+mqtt_receiver sound tts notify statefiles
+
+#wait for all processes to end
+wait
