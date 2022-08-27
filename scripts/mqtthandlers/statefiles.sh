0feb3a130a859ce6fe697cb21978b539cec64bef
Maarten van Gompel <proycon@anaproy.nl>
2022-08-27 14:45:15 +0200

    statefiles: updated

--- scripts/mqtthandlers/statefiles.sh
+++ scripts/mqtthandlers/statefiles.sh
@@ -6,11 +6,16 @@
  
 handle_statefiles() {
     case $TOPIC in
-        "home/sensor/"*|"home/binary_sensor/"*)
+        "home/sensor/"*|"home/binary_sensor/"*|"home/lights/"*|"home/presence/"*|"home/climate/"*)
             TYPE=$(echo "$TOPIC" | cut -d "/" -f 2)
             SENSOR=$(echo "$TOPIC" | cut -d "/" -f 3)
             [ ! -e "$HASTATEDIR/$TYPE" ] && mkdir -p "$HASTATEDIR/$TYPE"
             echo "$PAYLOAD" > "$HASTATEDIR/$TYPE/$SENSOR"
+            return 0
+            ;;
+        "home/alarm")
+            SENSOR=$(echo "$TOPIC" | cut -d "/" -f 2)
+            echo "$PAYLOAD" > "$HASTATEDIR/$SENSOR"
             return 0
             ;;
         "home/musicplayer/get/$HOSTNAME"|"home/kodi/get/$HOSTNAME")
