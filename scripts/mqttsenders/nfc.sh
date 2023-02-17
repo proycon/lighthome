8a5e96f9e503f558e442de9f6117464b8ebf0dde
Maarten van Gompel <proycon@anaproy.nl>
2023-02-17 22:51:10 +0100

    run nfc-daemon via sudo for now

--- scripts/mqttsenders/nfc.sh
+++ scripts/mqttsenders/nfc.sh
@@ -7,9 +7,13 @@ fi
 . "$HAROOT/scripts/common/include.sh"
 
 havedep nfc-daemon
+NFCDAEMON=$(which nfc-daemon)
+
+#uses a specificial sudoers rule to run nfc-daemon with sufficient privileges to access USB
+# homeautomation ALL=(root) NOPASSWD: /usr/local/bin/nfc-daemon
 
 #shellcheck disable=SC2086
-nfc-daemon | while read -r line
+sudo -n "$NFCDAEMON" | while read -r line
 do
     if echo "$line" | grep -q UID; then
         UID="$(echo "$line" | sed 's/UID=//')"
