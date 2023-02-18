3ffa9db92dd6d1ef6e566c5b775def2dbe7294ea
Maarten van Gompel <proycon@anaproy.nl>
2023-02-18 11:23:20 +0100

    added video player handler

--- pi2.sh
+++ pi2.sh
@@ -24,7 +24,7 @@ export MPD_HOST="192.168.0.1"
 export DEFAULT_MPC_SEARCH="Instrumental/Calm Piano"
 
 #runs asynchronously, calls specified handlers
-mqtt_receiver 433send sound musicplayer tts kodi technofire irsend hdmi_cec_send statefiles
+mqtt_receiver 433send sound video musicplayer tts kodi technofire irsend hdmi_cec_send statefiles
 
 #runs asynchronously, calls specified sender
 #         TOPIC
---
+++ scripts/mqtthandlers/video.sh
@@ -0,0 +1,106 @@
+#!/bin/sh
+
+#take care to run everything asynchronously from this point onward!
+
+havevar HOSTNAME
+havedep yt-dlp
+havedep playerctl #MPRIS
+
+[ -z "$PLAYVIDEO" ] && PLAYVIDEO="cvlc --play-and-exit"
+havedep "$(echo "$PLAYVIDEO" | cut -d " " -f 1)"
+
+
+clearqueue() {
+    playerctl stop
+    killall "$(echo "$PLAYVIDEO" | cut -d " " -f 1)"
+    mkdir "$TMPDIR/videoqueue/"
+    rm "$TMPDIR/videoqueue/"*
+}
+
+download() {
+    mkdir -p "$TMPDIR/videoqueue/"
+    cd "$TMPDIR/videoqueue/" || error "videoqueue not created"
+    "$HAROOT/scripts/voice/picotts.sh" "Downloading video" &
+    yt-dlp "$PAYLOAD" || (sleep 3 && "$HAROOT/scripts/voice/picotts.sh" "Download failed")
+}
+
+playqueue() {
+    $PLAYVIDEO "$TMPDIR/videoqueue/"* || "$HAROOT/scripts/voice/picotts.sh" "Video playback failed"
+}
+
+handle_sound() {
+    case $TOPIC in
+        "home/video/$HOSTNAME/clear")
+            clearqueue
+            return 0
+            ;;
+        "home/video/$HOSTNAME/pause")
+            playerctl play-pause &
+            return 0
+            ;;
+        "home/video/$HOSTNAME/stop")
+            playerctl stop && clearqueue
+            return 0
+            ;;
+        "home/video/$HOSTNAME/next")
+            playerctl next
+            return 0
+            ;;
+        "home/video/$HOSTNAME/forward")
+            playerctl position 30+
+            return 0
+            ;;
+        "home/video/$HOSTNAME/rewind")
+            playerctl position 30-
+            return 0
+            ;;
+        "home/video/$HOSTNAME/previous")
+            playerctl previous
+            return 0
+            ;;
+        "home/video/$HOSTNAME/loop")
+            playerctl loop Track && "$HAROOT/scripts/voice/picotts.sh" "Loop enabled" &
+            return 0
+            ;;
+        "home/video/$HOSTNAME/noloop")
+            playerctl loop None && "$HAROOT/scripts/voice/picotts.sh" "Loop disabled" &
+            return 0
+            ;;
+        "home/video/$HOSTNAME/add")
+            case $PAYLOAD in
+                http*)
+                    download "$PAYLOAD" &
+                    ;;
+            esac
+            ;;
+        "home/video/$HOSTNAME/play")
+            case $PAYLOAD in
+                "")
+                    "$HAROOT/scripts/voice/picotts.sh" "Playing existing video queue" &
+                    playqueue &
+                    ;;
+                http*)
+                    clearqueue
+                    (download "$PAYLOAD" && playqueue) &
+                    ;;
+                *mp4|*avi|*webm|*ogv)
+                    FILENAME="$HAROOT/media/$PAYLOAD"
+                    if [ -e "$FILENAME" ]; then
+                        $PLAY "$FILENAME" &
+                    else
+                        "$HAROOT/scripts/voice/picotts.sh" "Video file not found" &
+                        error "Unable to play $FILENAME, file not found"
+                    fi
+                    return 0
+                    ;;
+                *)
+                    "$HAROOT/scripts/voice/picotts.sh" "Unknown video" &
+                    error "Unknown video payload"
+                    ;;
+            esac
+            ;;
+        *)
+            return 9
+            ;;
+    esac
+}
