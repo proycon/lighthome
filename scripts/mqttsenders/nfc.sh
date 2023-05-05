7fa834c22f92ab9a917655f15ee2d516340b73d1
Maarten van Gompel <proycon@anaproy.nl>
2023-05-05 21:51:13 +0200

    debug

--- scripts/mqttsenders/nfc.sh
+++ scripts/mqttsenders/nfc.sh
@@ -14,14 +14,18 @@ NFCDAEMON=$(which nfc-daemon)
 
 #shellcheck disable=SC2086
 sleep 5 #delay to prevent Unable to write to USB error when reconnecting rapidly
-sudo -n "$NFCDAEMON" 2> /tmp/nfc.err.log | tee /tmp/nfc.out.log | while read -r line
+info "nfc starting daemon"
+sudo -n "$NFCDAEMON" | while read -r line
 do
     if echo "$line" | grep -q UID; then
         UID="$(echo "$line" | sed 's/UID=//')"
         info "nfc IN: $UID"
         mqttpub "home/nfc" "$UID"
+    elif [ -n "$line" ]; then
+        info "nfc (unhandled): $line"
     fi
 done
 RET=$?
+info "nfc daemon stopped"
 
 exit $RET
