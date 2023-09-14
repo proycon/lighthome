33bd4c74a58d8ea4ffb1f9f597b3c1ffde7116c5
Maarten van Gompel <proycon@anaproy.nl>
2023-09-14 21:56:07 +0200

    numen debug

--- startnumen.sh
+++ startnumen.sh
@@ -7,6 +7,6 @@ if [ "$HOST" = "pi1" ]; then
     export PLAY="mpv --audio-device=alsa/sysdefault:CARD=Device --no-video --really-quiet"
 fi
 
-./send.sh home/say/pi1 "Voice control on"
-numen ~/lighthome/config/house.phrases
-./send.sh home/say/pi1 "Voice control off"
+./send.sh home/say/pi1 "Voice control enabled"
+numen --phraselog=/dev/stdout ~/lighthome/config/house.phrases
+./send.sh home/say/pi1 "Voice control disabled"
