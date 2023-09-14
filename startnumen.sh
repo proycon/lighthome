d1de94f22ca3ec0b29c2aec61093b816296339bc
Maarten van Gompel <proycon@anaproy.nl>
2023-09-14 22:07:05 +0200

    added MQTT_SESSION_SUFFIX

--- startnumen.sh
+++ startnumen.sh
@@ -7,6 +7,7 @@ if [ "$HOST" = "pi1" ]; then
     export PLAY="mpv --audio-device=alsa/sysdefault:CARD=Device --no-video --really-quiet"
 fi
 
+export MQTT_SESSION_SUFFIX=.send
 ./send.sh home/say/pi1 "Voice control enabled"
 numen --phraselog=/dev/stdout ~/lighthome/config/house.phrases
 ./send.sh home/say/pi1 "Voice control disabled"
