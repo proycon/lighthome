429e18f6220c8be511a9edafa6e5e4900e43d183
Maarten van Gompel <proycon@anaproy.nl>
2023-09-15 10:08:51 +0200

    debug

--- scripts/numen/numen_wake.sh
+++ scripts/numen/numen_wake.sh
@@ -3,6 +3,6 @@ PHRASES=~/lighthome/config/house.phrases
 echo "numen wake">&2
 echo "load $PHRASES"
 ./send.sh home/say/$(hostname) "yes?" >/dev/null 2>/dev/null
-if [ -n "$1" ]; then
-    (sleep $1; ~/lighthome/scripts/numen/numen_idle.sh --silent) &
-fi
+#if [ -n "$1" ]; then
+#    (sleep $1; ~/lighthome/scripts/numen/numen_idle.sh --silent) &
+#fi
