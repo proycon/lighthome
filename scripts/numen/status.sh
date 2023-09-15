0b380dd4b6880351c2f4f4b0a423ccd1766f897b
Maarten van Gompel <proycon@anaproy.nl>
2023-09-15 13:37:44 +0200

    delays

--- scripts/numen/status.sh
+++ scripts/numen/status.sh
@@ -10,13 +10,19 @@ if [ "$(cat /tmp/homestatus/presence/proycon)" = "ON" ]; then
 fi
 if [ $HANS -eq 0 ]; then
     ~/lighthome/send.sh home/say/$(hostname) "Hans is away"
+    sleep 3
 fi
 if [ $MAARTEN -eq 0 ]; then
     ~/lighthome/send.sh home/say/$(hostname) "Maarten is away"
+    sleep 3
 fi
 if grep -qi ON /tmp/homestatus/binary_sensor/*; then
     ~/lighthome/send.sh home/say/$(hostname) "there are doors or windows open"
+    sleep 4
 fi
 ~/lighthome/send.sh home/say/$(hostname) "central heating: $(cat /tmp/homestatus/climate/cv)"
+sleep 4
 ~/lighthome/send.sh home/say/$(hostname) "climate control downstairs: $(cat /tmp/homestatus/climate/aircobeneden)"
+sleep 4
 ~/lighthome/send.sh home/say/$(hostname) "climate control upstairs: $(cat /tmp/homestatus/climate/aircoboven)"
+sleep 4
