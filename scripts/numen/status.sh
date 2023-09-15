59215fde96402a2c244525901a9245554173bd7e
Maarten van Gompel <proycon@anaproy.nl>
2023-09-15 13:36:04 +0200

    voice: status

--- scripts/numen/status.sh
+++ scripts/numen/status.sh
@@ -9,11 +9,14 @@ if [ "$(cat /tmp/homestatus/presence/proycon)" = "ON" ]; then
     MAARTEN=1
 fi
 if [ $HANS -eq 0 ]; then
-    run ~/lighthome/send.sh home/say/$(hostname) "Hans is away"
+    ~/lighthome/send.sh home/say/$(hostname) "Hans is away"
 fi
 if [ $MAARTEN -eq 0 ]; then
-    run ~/lighthome/send.sh home/say/$(hostname) "Maarten is away"
+    ~/lighthome/send.sh home/say/$(hostname) "Maarten is away"
 fi
-if grep -q ON /tmp/homestatus/binary_sensor/*; then
-    run ~/lighthome/send.sh home/say/$(hostname) "there are doors or windows open"
+if grep -qi ON /tmp/homestatus/binary_sensor/*; then
+    ~/lighthome/send.sh home/say/$(hostname) "there are doors or windows open"
 fi
+~/lighthome/send.sh home/say/$(hostname) "central heating: $(cat /tmp/homestatus/climate/cv)"
+~/lighthome/send.sh home/say/$(hostname) "climate control downstairs: $(cat /tmp/homestatus/climate/aircobeneden)"
+~/lighthome/send.sh home/say/$(hostname) "climate control upstairs: $(cat /tmp/homestatus/climate/aircoboven)"
