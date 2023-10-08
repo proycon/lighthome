6168801069f132aef632cf748af9be0ea3207820
Maarten van Gompel <proycon@anaproy.nl>
2023-10-08 14:36:55 +0200

    revert

--- scripts/numen/numen_wake.sh
+++ scripts/numen/numen_wake.sh
@@ -3,5 +3,5 @@ PHRASES=~/lighthome/config/house.phrases
 echo "load $PHRASES"
 touch "$NUMEN_STATE_DIR/listening"
 ./send.sh home/say/$(hostname) "yes?" >/dev/null 2>/dev/null
-pkill -f numen_idle.sh 2> /dev/null
-(sleep 7 && ~/lighthome/scripts/numen/numen_idle.sh --silent) &
+#pkill -f numen_idle.sh 2> /dev/null
+#(sleep 7 && ~/lighthome/scripts/numen/numen_idle.sh --silent) &
