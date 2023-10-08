7677dca26d662715195147921768950308435fe0
Maarten van Gompel <proycon@anaproy.nl>
2023-10-08 14:34:24 +0200

    numen: return to idle state automatically after some time

--- scripts/numen/numen_wake.sh
+++ scripts/numen/numen_wake.sh
@@ -3,6 +3,5 @@ PHRASES=~/lighthome/config/house.phrases
 echo "load $PHRASES"
 touch "$NUMEN_STATE_DIR/listening"
 ./send.sh home/say/$(hostname) "yes?" >/dev/null 2>/dev/null
-#if [ -n "$1" ]; then
-#    (sleep $1; ~/lighthome/scripts/numen/numen_idle.sh --silent) &
-#fi
+pkill -f numen_idle.sh 2> /dev/null
+(sleep 7 && ~/lighthome/scripts/numen/numen_idle.sh --silent) &
