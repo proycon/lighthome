c6dd4c9750a34f427f42242390b32edaf6c81cf2
Maarten van Gompel <proycon@anaproy.nl>
2024-05-20 16:50:53 +0200

    don't use sudo for NFC reader, use proper udev rule

--- scripts/mqttsenders/nfc.sh
+++ scripts/mqttsenders/nfc.sh
@@ -9,13 +9,13 @@ fi
 havedep nfc-daemon
 NFCDAEMON=$(which nfc-daemon)
 
-#uses a specificial sudoers rule to run nfc-daemon with sufficient privileges to access USB
-# homeautomation ALL=(root) NOPASSWD: /usr/local/bin/nfc-daemon
+#uses a specificial udev rule to run nfc-daemon with sufficient privileges to access USB
+# ATTRS{idVendor}=="072f", ATTRS{idProduct}=="2200", SUBSYSTEMS=="usb", ACTION=="add", MODE="0666", GROUP="plugdev"
 
 #shellcheck disable=SC2086
 sleep 5 #delay to prevent Unable to write to USB error when reconnecting rapidly
 info "nfc starting daemon"
-sudo -n "$NFCDAEMON" | while read -r line
+"$NFCDAEMON" | while read -r line
 do
     if echo "$line" | grep -q UID; then
         UID="$(echo "$line" | sed 's/UID=//')"
